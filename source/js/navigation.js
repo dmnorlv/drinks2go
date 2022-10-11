da18d1d756d80c2b7d74c5c4bf6456f4cc2aaa39
dmnorlv <dmnorlv21@gmail.com>
2022-10-12 00:07:56 +0500

    слайдер v0.2

--- source/js/navigation.js
+++ source/js/navigation.js
@@ -1,10 +1,13 @@
 const mainMenu = document.querySelector('.menu__main');
 const headerButton = document.querySelector('.header__button');
+const mainIndex = document.querySelector('.index__main')
 
 mainMenu.classList.add('menu__main--js');
 headerButton.classList.add('header__button--js');
+mainIndex.classList.add('index__main--js');
 
 headerButton.addEventListener('click', () => {
+  mainIndex.classList.toggle('index__main--js');
   mainMenu.classList.toggle('menu__main--js');
   headerButton.classList.toggle('header__button--js');
 })
