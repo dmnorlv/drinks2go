11bfa7f3eeac31b09dbf46425d940410aa4e788d
dmnorlv <dmnorlv21@gmail.com>
2022-10-16 12:10:34 +0500

    карта v0.2

--- source/js/map.js
+++ source/js/map.js
@@ -10,3 +10,22 @@ L.tileLayer(
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   },
 ).addTo(map);
+
+
+const mainPinIcon = L.icon({
+  iconUrl: './img/map-pin.svg',
+  iconSize: [52, 52],
+  iconAnchor: [26, 52],
+});
+
+const mainPinMarker = L.marker(
+  {
+    lat: 59.96831,
+    lng: 30.31748,
+  },
+  {
+    icon: mainPinIcon,
+  },
+);
+
+mainPinMarker.addTo(map);
